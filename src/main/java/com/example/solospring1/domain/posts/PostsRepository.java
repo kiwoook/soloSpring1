cd692e1312b9185a9edf1bc557fedee89f769cb7
kiwoook <tizmfns1218@naver.com>
2022-08-18 13:02:07 +0900

    변경

---
+++ src/main/java/com/example/solospring1/domain/posts/PostsRepository.java
@@ -0,0 +1,13 @@
+package com.example.solospring1.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.*;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.solospring1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.solospring1 to javafx.fxml;
-    exports com.example.solospring1;
-}
