57dea91bdfe9f6ac2afa5b54a9e2450c3c2eb3eb
kiwoook <tizmfns1218@naver.com>
2022-08-19 21:45:08 +0900

    변경

--- src/test/java/com/example/solospring1/web/IndexControllerTest.java
+++ src/test/java/com/example/solospring1/web/IndexControllerTest.java
@@ -12,15 +12,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IndexControllerTest {
+
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void 메인페이지_로딩(){
+    public void 메인페이지_로딩() {
         //when
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
