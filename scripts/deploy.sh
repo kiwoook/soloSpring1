9cf716b2e6c16e0eeff59deacfdac2f08afcb779
kiwoook <tizmfns1218@naver.com>
2022-08-26 22:23:25 +0900

    변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,21 @@
-#! /bin/bash
+#!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=freelec-springboot2-webservice
+PROJECT_NAME=RestApiPractice
 
-echo "> Buuild 파일 복사"
+cd $REPOSITORY/$PROJECT_NAME
+
+echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl RestApiPractice | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID"]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
@@ -23,9 +25,9 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | grep -v "plain" | tail -n 1)
 
-echo "> JAR Name: $JAR_NAME"
+echo "> JAR_NAME: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
@@ -34,6 +36,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-  -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+        -Dspring.config.location=classpath:/application.yaml,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+        -Dspring.profile.active=real \
+$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
